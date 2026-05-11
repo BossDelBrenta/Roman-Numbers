154d274853573eed359ed6e8b98bd37bb2ee2678
Alessandro Cisco <alessandro.cisco05@gmail.com>
2026-05-11 12:22:23 +0200

    feat: conversione completa 1-1000 con validazione - closes #7

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -6,7 +6,7 @@ package it.unipd.mtss;
 
 /**
  * Classe per la conversione di numeri arabi in numeri romani.
- * Supporta la conversione di numeri interi positivi.
+ * Supporta la conversione di numeri interi da 1 a 1000.
  *
  * @author Alessandro Cisco
  * @author Enrico Biasini
@@ -14,17 +14,18 @@ package it.unipd.mtss;
 public class IntegerToRoman {
 
     private static final int[] VALUES = {
-        100, 90, 50, 40, 10, 9, 5, 4, 1
+        1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1
     };
 
     private static final String[] SYMBOLS = {
-        "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"
+        "M", "CM", "D", "CD", "C", "XC",
+        "L", "XL", "X", "IX", "V", "IV", "I"
     };
 
     /**
      * Converte un numero arabo nella sua rappresentazione romana.
      *
-     * @param number il numero intero da convertire
+     * @param number il numero intero da convertire (1-1000)
      * @return la stringa contenente il numero romano
      * @throws IllegalArgumentException se il numero e' fuori range
      */
@@ -35,9 +36,9 @@ public class IntegerToRoman {
                 + "ricevuto: " + number
             );
         }
-        if (number > 100) {
+        if (number > 1000) {
             throw new IllegalArgumentException(
-                "Il numero deve essere minore o uguale a 100, "
+                "Il numero deve essere minore o uguale a 1000, "
                 + "ricevuto: " + number
             );
         }
